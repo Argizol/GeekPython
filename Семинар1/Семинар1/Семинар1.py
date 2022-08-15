ce520f7c460437bd8016799f26c38f3c52be2f4d
Dmitriy <argizol@mail.ru>
2022-08-15 21:43:44 +0300

    Дополнено заданием 10

--- Семинар1/Семинар1/Семинар1.py
+++ Семинар1/Семинар1/Семинар1.py
@@ -101,3 +101,29 @@ def whichCoords(numOfQuadro):
         if numOfQuadro == 3:
             return "От -1 до -бесконечности, y = От 1 до бесконесности"
 print(whichCoords(numOfQuadro))
+
+#Задание 10
+import math
+
+
+print('Введите координаты первой точки')
+print('x1 =')
+x1 = int(input())
+print('y1 =')
+y1 = int(input())
+print('z1 =')
+z1 = int(input())
+print('x2 =')
+x2 = int(input())
+print('y2 =')
+y2 = int(input())
+print('z2 =')
+z2 = int(input())
+point1 = (x1, y1, z1)
+point2 = (x2, y2, z2)
+
+def DistanceBetweenPoint(point1, point2):
+
+    return math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2) + ((z2 - z1) ** 2))     
+
+print(DistanceBetweenPoint(point1, point2))
