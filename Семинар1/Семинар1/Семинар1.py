57d592d57f631040f2b5c2d89c474cc01ef36bc9
Dmitriy <argizol@mail.ru>
2022-08-29 20:35:18 +0300

    HW5

--- Семинар1/Семинар1/Семинар1.py
+++ Семинар1/Семинар1/Семинар1.py
@@ -560,21 +560,66 @@ from unicodedata import decimal
 #  многочлена и записать в файл многочлен степени k. 
 #  Пример: k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-k = int(input('Введите макисмальный коэффициент степени для многочлена: '))
-resultString = ''
-if k >= 0:
-    while k > 0:
-        if k == 1:
-            resultString += f'{randint(0, 100)}*x + '
-        else:
-            resultString += f'{randint(0, 100)}*x^{k} + '
-        k -= 1
+#k = int(input('Введите макисмальный коэффициент степени для многочлена: '))
+#resultString = ''
+#if k >= 0:
+#    while k > 0:
+#        if k == 1:
+#            resultString += f'{randint(0, 100)}*x + '
+#        else:
+#            resultString += f'{randint(0, 100)}*x^{k} + '
+#        k -= 1
 
+#else:
+#    while k < 0:
+#        resultString += f'{randint(0, 100)}*x^{k} + '
+#        k += 1
+#resultString += f'{randint(0, 100)} = 0'
+#with open(r'hw4.txt','+a') as f:
+#    f.write(resultString + '\n')
+#print(resultString)
+
+##Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+##str1 = list(input().split())
+#str1 = ['абв', 'пдлдляв', 'абвщлли', 'ащдлпоь']
+#str2 = [str1[x] for x in range(len(str1)) if not str1[x].__contains__("абв")]
+#strResult = ''
+#for i in str2:
+#    strResult += i + ' '
+#print(strResult.strip())
+
+#Создайте программу для игры с конфетами человек против человека.
+#Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+#a) Добавьте игру против бота
+#b) Подумайте как наделить бота ""интеллектом""
+
+numsOfCandies = 2021
+counter = 0
+def playerTurn(numsOfCandies):
+    if numsOfCandies == 0:
+        return -1
+    takenCandies = int(input("Введите сколько конфет вы забираете: "))
+    while takenCandies <= 0 or takenCandies > 28:
+        takenCandies = int(input('Брать можно только от 1 до 28 конфет. Попробуйте снова: '))
+        if numsOfCandies < takenCandies:
+             print(f'осталось {numsOfCandies} конфет, введите число от 1 до {numsOfCandies}')
+    if numsOfCandies < takenCandies:
+        while takenCandies <= 0 or takenCandies > numsOfCandies:    
+            takenCandies = int(input(f'осталось {numsOfCandies} конфет, введите число от 1 до {numsOfCandies}: '))    
+    numsOfCandies -= takenCandies   
+    return numsOfCandies
+
+while numsOfCandies > 0:
+        if numsOfCandies > 0:
+            print('Ход игрока 1')
+            numsOfCandies = playerTurn(numsOfCandies)
+            counter += 1
+        if numsOfCandies > 0:
+            print('Ход игрока 2')
+            numsOfCandies = playerTurn(numsOfCandies)
+            counter += 1
+if counter % 2 == 0:
+    print('Победил игрок 2')
 else:
-    while k < 0:
-        resultString += f'{randint(0, 100)}*x^{k} + '
-        k += 1
-resultString += f'{randint(0, 100)} = 0'
-with open(r'hw4.txt','+a') as f:
-    f.write(resultString + '\n')
-print(resultString)
+    print('Победил игрок 1')
